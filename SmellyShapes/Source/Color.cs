18d2f9b1d9dfbb97fe9423c332ed0fdb3095cc7a
Christian Abegg <Christian.Abegg@zuehlke.com>
2022-04-07 16:47:26 +0200

    02 (magic string) 01: introduce constant (Ctrl-R, V) Red in Color

--- SmellyShapes/Source/Color.cs
+++ SmellyShapes/Source/Color.cs
@@ -2,6 +2,7 @@ namespace SmellyShapes.Source;
 
 public class Color
 {
+    private const string Red = "Red";
     private readonly string colorAsText;
 
     public Color(string colorAsText)
@@ -36,7 +37,7 @@ public class Color
         ErrorMessage = string.Empty;
 
         // set to Red
-        if (colorAsText == "Red")
+        if (colorAsText == Red)
         {
             ColorAsRgbRed = "255";
             ColorAsRgbBlue = "0";
