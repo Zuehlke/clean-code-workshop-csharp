1bf2cf58a95d72308fb599963f8cbc058c061138
Christian Abegg <Christian.Abegg@zuehlke.com>
2022-11-18 11:38:17 +0100

    use backing field and file-scoped namespace

--- SmellyShapes/Source/Circle.cs
+++ SmellyShapes/Source/Circle.cs
@@ -1,48 +1,52 @@
-namespace SmellyShapes.Source
+namespace SmellyShapes.Source;
+
+public class Circle : SimpleShape
 {
-    public class Circle : SimpleShape
+    private int numberOfContainingPoints;
+    private readonly int x;
+    private readonly int y;
+
+    public Circle(int x, int y, int radius)
     {
-        private int numberOfContainingPoints;
+        this.x = x;
+        this.y = y;
+        this.Radius = radius;
+    }
 
-        public Circle(int x, int y, int radius)
-        {
-            this.X = x;
-            Y = y;
-            this.Radius = radius;
-        }
+    /// <summary>
+    ///     The shape color
+    /// </summary>
+    public Color Color { get; set; } = new Color("Green");
 
-        /// <summary>
-        ///     The shape color
-        /// </summary>
-        public Color Color { get; set; } = new Color("Green");
+    public int X
+    {
+        get { return this.x; }
+    }
 
-        public int X { get; }
+    public int Y => this.y;
 
-        public int Y { get; }
+    public int Radius { get; }
 
-        public int Radius { get; }
+    public override bool Contains(int x, int y)
+    {
+        var result = (x - this.X) * (x - this.X) + (y - Y) * (y - Y) <= Radius * Radius;
+        // Increase number of Points?
+        if (result) numberOfContainingPoints++;
+        return result;
+    }
 
-        public override bool Contains(int x, int y)
-        {
-            var result = (x - this.X) * (x - this.X) + (y - Y) * (y - Y) <= Radius * Radius;
-            // Increase number of Points?
-            if (result) numberOfContainingPoints++;
-            return result;
-        }
+    public int CountContainingPoints(int[] xCords, int[] yCords)
+    {
+        numberOfContainingPoints = 0;
+        for (var i = 0; i < xCords.Length; ++i) Contains(xCords[i], yCords[i]);
+        return numberOfContainingPoints;
+    }
 
-        public int CountContainingPoints(int[] xCords, int[] yCords)
-        {
-            numberOfContainingPoints = 0;
-            for (var i = 0; i < xCords.Length; ++i) Contains(xCords[i], yCords[i]);
-            return numberOfContainingPoints;
-        }
-
-        public override string ToString()
-        {
-            return "Circle: (" + X + "," + Y + ") radius= " + Radius
-                   + " RGB=" + Color.ColorAsRGBRed + ","
-                   + Color.ColorAsRGBGreen + ","
-                   + Color.ColorAsRGBBlue;
-        }
+    public override string ToString()
+    {
+        return "Circle: (" + X + "," + Y + ") radius= " + Radius
+               + " RGB=" + Color.ColorAsRGBRed + ","
+               + Color.ColorAsRGBGreen + ","
+               + Color.ColorAsRGBBlue;
     }
 }
