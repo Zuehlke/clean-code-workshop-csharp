4afaf70c896a26f1dcc30681ccc5ca325794a3e4
Fabian Meier <fabian.meier@zuehlke.com>
2023-05-16 13:26:23 +0200

    remove DrawingBoard.NoDate because it is never used in an exercise

--- SmellyShapes/Source/DrawingBoard.cs
+++ SmellyShapes/Source/DrawingBoard.cs
@@ -2,11 +2,6 @@ namespace SmellyShapes.Source
 {
     public class DrawingBoard : ShapeGroup
     {
-        /// <summary>
-        ///     Use NoDate to indicate that no date is set (the equivalent of 'null Date' in Java).
-        /// </summary>
-        public static readonly DateTime NoDate = DateTime.MaxValue;
-
         public Color BackgroundColor { get; set; }
 
         public static void Main()
@@ -23,10 +18,6 @@ namespace SmellyShapes.Source
 
         public void DrawOnScreen()
         {
-            var previousStart = NoDate;
-            var thisStart = DateTime.Now;
-            Console.WriteLine("Started DrawOnScreen last time: " + previousStart);
-
             // ... removed for exercise
         }
 
