105ac3257645d8e773c93d74e80d5a63bd16c67d
Fabian Meier <fabian.meier@zuehlke.com>
2024-05-24 11:11:10 +0200

    use ShapeToXmlConverter.Convert in ShapeToXmlConverter.ToXmlStatic(Circle)

--- SmellyShapes/Source/ShapeToXmlConverter.cs
+++ SmellyShapes/Source/ShapeToXmlConverter.cs
@@ -29,14 +29,6 @@ public class ShapeToXmlConverter : IShapeVisitor<string>
 
     public static string ToXmlStatic(Circle circle)
     {
-        var builder = new StringBuilder();
-
-        builder.Append("<circle");
-        builder.Append(" x=\"" + circle.Center.X + "\"");
-        builder.Append(" y=\"" + circle.Center.Y + "\"");
-        builder.Append(" radius=\"" + circle.Radius + "\"");
-        builder.Append(" />\n");
-
-        return builder.ToString();
+        return Convert(circle);
     }
 }
