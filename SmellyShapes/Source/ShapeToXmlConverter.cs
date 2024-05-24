07d031d979d197412a0b52369db0aec47521c474
Fabian Meier <fabian.meier@zuehlke.com>
2024-05-24 11:03:20 +0200

    inline ShapeToXmlConverter.ToXmlStatic(Circle) into ShapeToXmlConverter.Visit(Circle)

--- SmellyShapes/Source/ShapeToXmlConverter.cs
+++ SmellyShapes/Source/ShapeToXmlConverter.cs
@@ -6,7 +6,15 @@ public class ShapeToXmlConverter : IShapeVisitor<string>
 {
     public string Visit(Circle circle)
     {
-        return ToXmlStatic(circle);
+        var builder = new StringBuilder();
+
+        builder.Append("<circle");
+        builder.Append(" x=\"" + circle.Center.X + "\"");
+        builder.Append(" y=\"" + circle.Center.Y + "\"");
+        builder.Append(" radius=\"" + circle.Radius + "\"");
+        builder.Append(" />\n");
+
+        return builder.ToString();
     }
 
     public static string ToXmlStatic(Circle circle)
